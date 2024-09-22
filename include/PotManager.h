4095d3a078d64153b18932b91dc060e060354814
Jimmy Chan <z5478376@ad.unsw.edu.au>
2024-09-22 15:10:09 +1000

    Implementation of PotManager and pot creation logic

---
+++ include/PotManager.h
@@ -0,0 +1,64 @@
+#ifndef POT_MANAGER_H
+#define POT_MANAGER_H
+
+#include "Player.h"
+#include <set>
+#include <map>
+using namespace std;
+
+typedef struct Pot {
+    size_t chips;
+    vector<shared_ptr<Player>> eligiblePlayers;
+
+    Pot();
+    void addPlayer(const shared_ptr<Player>& player);
+    void addChips(size_t amount);
+    size_t getChips() const;
+    const vector<shared_ptr<Player>>& getEligiblePlayers() const;
+} Pot;
+
+class PotManager {
+private:
+    // Vector of all pots
+    vector<Pot> pots;
+
+    // Maps players to their most recent bets in a betting street
+    map<shared_ptr<Player>, size_t> playerBets;
+
+    // Amount of dead chips from folded players.
+    size_t deadChips;
+
+    // Helper function to add a new side pot.
+    void newPot();
+
+    // Helper function to fetch the current pot.
+    Pot& getCurPot();
+
+    // Helper function to check if all bets have been placed in pots.
+    bool allPotsCreated() const;
+
+    // Helper function to find the minimum bet of all players.
+    size_t findMinBet() const;
+
+    // Display pots information
+    void displayPots();
+
+public:
+    // Initalises a single pot.
+    PotManager();
+
+
+    // Updates the playerBets map after a bet/raises. Called after each player action.
+    void addPlayerBet(const shared_ptr<Player>& player, size_t bet);
+
+    // Increments dead money and sets folded player's recent bet to 0.
+    void foldPlayerBet(const shared_ptr<Player>& player);
+
+    // Calculates pots after betting action in a street is finished. Called after each street
+    void calculatePots();
+
+    // Resets recent bets and dead money to 0. Called after each street.
+    void resetPlayerBets();
+};
+
+#endif // POT_MANAGER_H
---
+++ src/PotManager.cpp
@@ -0,0 +1,136 @@
+#include "../include/PotManager.h"
+#include <limits>
+#include <iostream>
+
+// Helper Functions
+
+void PotManager::newPot() {
+    pots.emplace_back();
+    cout << "New pot created!" << endl;
+}
+
+Pot& PotManager::getCurPot() {
+    if (pots.empty()) throw runtime_error("No pots available to fetch!");
+    return pots.back();
+}
+
+bool PotManager::allPotsCreated() const {
+    for (auto& [player, recentBet] : playerBets) {
+        if (recentBet != 0) return false;
+    }
+    return true;
+}
+
+size_t PotManager::findMinBet() const { 
+    size_t minBet = numeric_limits<size_t>::max();
+    for (auto& [player, recentBet] : playerBets) {
+        if (recentBet < minBet)  minBet = recentBet;
+    }
+    return minBet;
+}
+
+void PotManager::displayPots() {
+    for (size_t i = 0; i < pots.size(); ++i) {
+        const Pot& pot = pots[i];
+
+        cout << "Pot " << i + 1 << ": " << pot.getChips() << " chips" << endl;
+        if (pot.getEligiblePlayers().empty()) {
+            cout << "No players in this pot!" << endl;
+        } else {
+            cout << "The following players are eligible for Pot " << i + 1 << endl;
+            for (const auto& player : pot.getEligiblePlayers()) {
+                cout << "        Player: " << player->getName();
+            }
+        }
+
+    }
+}
+
+// Pot Manager
+
+PotManager::PotManager() : deadChips(0) {
+    newPot();
+}
+
+void PotManager::addPlayerBet(const shared_ptr<Player>& player, size_t bet) {
+    auto it = playerBets.find(player);
+
+    // Add player and their bet if they don't exist
+    if (it == playerBets.end()) {
+        playerBets[player] = bet;
+    }
+    // If player already exists, simply update their recent bet
+    else {
+        it->second = bet;
+    }
+}
+
+void PotManager::foldPlayerBet(const shared_ptr<Player>& player) {
+    auto it = playerBets.find(player);
+
+    // If player has not made a bet, exit early.
+    // No need to change deadMoney or playerBets.
+    if (it == playerBets.end()) return;
+    
+    // Increment dead money in pot
+    deadChips += playerBets[player];
+
+    // Update folded player's recent bet to 0
+    playerBets[player] = 0;
+}
+
+void PotManager::calculatePots() {
+    // Suppose playerBets is populated with player's and their bets at the end of a street.
+
+    while (!allPotsCreated()) {
+        size_t minBet = findMinBet();
+        Pot& curPot = getCurPot();
+
+        // Add each player's contribution to the current pot
+        for (auto& [player, recentBet] : playerBets) {
+            if (recentBet == 0) continue;
+            playerBets[player] -= minBet;
+            curPot.addChips(minBet);
+            curPot.addPlayer(player);
+        }
+
+        // Add dead money to the current pot
+        if (deadChips > minBet) {
+            curPot.addChips(minBet);
+            deadChips -= minBet;
+        } else {
+            curPot.addChips(deadChips);
+            deadChips = 0;
+        }
+
+        // Create a side pot if required
+        if (!allPotsCreated()) newPot();
+    }
+}
+
+void PotManager::resetPlayerBets() {
+    for (auto& [player, recentBet] : playerBets) {
+        playerBets[player] = 0;
+    }
+}
+
+// Pot Structure
+
+Pot::Pot() : chips(0) {}
+
+void Pot::addPlayer(const shared_ptr<Player>& player) {
+    eligiblePlayers.push_back(player);
+}
+
+void Pot::addChips(size_t amount) {
+    chips += amount;
+}
+
+size_t Pot::getChips() const {
+    return chips;
+}
+
+const vector<shared_ptr<Player>>& Pot::getEligiblePlayers() const {
+    return eligiblePlayers;
+}
+
