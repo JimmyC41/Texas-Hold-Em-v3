6ce0102c739c045fb986457cd5cf53a259a0940a
Jimmy Chan <z5478376@ad.unsw.edu.au>
2024-10-06 20:59:40 +1100

    Comments for game flow logic

--- include/GameController.h
+++ include/GameController.h
@@ -13,6 +13,14 @@
 #include <memory.h>
 using namespace std;
 
+enum Street {
+    PRE_FLOP = 0,
+    FLOP,
+    TURN,
+    RIVER,
+    SHOWDOWN
+};
+
 class GameController {
 private:
     size_t smallBlind;
@@ -52,13 +60,18 @@ public:
     // Deals community cards to board
     void dealBoard(int numCards);
 
-    // STREET METHOD
-
+    // STREET SPECIFIC METHOD
+    
     // Check if all players have acted in a given round
     // TODO
     bool isStreetOver();
 
-    // 
+    // Start a new betting street
+    void startStreet(Street newStreet);
+
+    // Update game state for new street
+    // Reset actions and calculate pots
+    void updateStreetState();
 
     // Set first player to act
     // Gets the current player to act
@@ -67,16 +80,19 @@ public:
     // Add action to action timeline
     // Update the pot manager after each player action
     // Update active / folded players after each player action
-
+    
     // END OF STREET
     // ActionManager: Clear action timeline
     // PotManager: Calculate pots and reset bets
+
+    // ROUND METHODS
+
+    // OVERALL GAME METHODS
+
+    // END OF RD
     // TurnManager: Move folded players, rotate positions
 
-
     // runGameLoop: Manages overall game flow
-    // Called after players are added
-
     // advanceToNextStreet: In a given round, advances to the next street
     // showdown: Evaluates the winner and distributes the pot
     // isGameOver: Checks if the game is over (e.g. less than 2 players)
